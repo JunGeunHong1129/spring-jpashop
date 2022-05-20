7c4cc3637354ef0c7cc2e3ae4fcb9425eeaa80ea
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-20 17:44:12 +0900

    22_05_19 기본적인 엔티티 개발 중 커밋

---
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "item")
+@DiscriminatorColumn(name = "dtype")
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @OneToMany(mappedBy = "item")
+    private List<Item> categories = new ArrayList<>();
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @OneToMany(mappedBy = "order_item")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+}
