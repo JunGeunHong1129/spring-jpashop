2e8dab6681f48eb32d578556923bdb6590053f8b
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-24 11:43:36 +0900

    feat(/controller): MemberController 추가
    애플리케이션 아키텍쳐 구성중 MemberController 추가

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+
+
+}
