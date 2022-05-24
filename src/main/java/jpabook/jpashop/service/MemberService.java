8f506bbebf3ed16d0c6ba82028168a14de7d2f69
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-24 12:30:26 +0900

    feat(/service): MemberService 기능 구현
    MemberService 기능 구현

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,7 +1,15 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+
 }
