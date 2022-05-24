5f466c72c19559fdb0a288dec18f7082929b54f7
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-24 17:55:00 +0900

    build(/java/resources/scripts): test 디비 추가
    test 디비 추가

--- src/main/resources/scripts/full-init.sh
+++ src/main/resources/scripts/full-init.sh
@@ -2,7 +2,7 @@
 
 echo "Chat-Server Start to Initialize"
 
-mkdir -p -m 777  ./volume/postgresql/data/ && mkdir -p -m 777  ./volume/postgresql/data/
+mkdir -p -m 777  ./volume/postgresql_prod/data/ && mkdir -p -m 777  ./volume/postgresql_prod/data/ && mkdir -p -m 777  ./volume/postgresql_dev/data/ && mkdir -p -m 777  ./volume/postgresql_dev/data/
 
 echo "Chat-Server Volume Set Complete"
 
