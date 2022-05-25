507a5b4819c70aa43fd79f8dec8f8c68a785c821
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-25 17:37:08 +0900

    test(/src/test/java/jpabook/jpashop/domain/entity): Item 엔티티 유닛 테스트 ItemTest 추가
    ##Item 엔티티 유닛 테스트 ItemTest 추가
    
     - ItemTest
    	 - 재고량 예외 유닛 테스트 추가

---
+++ src/test/java/jpabook/jpashop/domain/entity/ItemTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain.entity;
+
+import jpabook.jpashop.exception.NotEnoughStockException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    @DisplayName("재고 삭제 예외 단위 테스트")
+    void removeStock() {
+
+        //given
+        Book book = createBookData("이펙티브 자바", 20000, 100);
+        int quantityVolume = 200;
+
+        //when
+        assertThrows(NotEnoughStockException.class, ()-> {
+            book.removeStock(quantityVolume);
+        });
+
+        //than
+//        fail("재고 수량 부족 예외 발생해야함");
+
+    }
+
+    private Book createBookData(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        return book;
+    }
+}
