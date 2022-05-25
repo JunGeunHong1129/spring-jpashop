0c6d2acd29555241363174904536119948210963
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-25 11:03:46 +0900

    feat(/src/main/java/jpabook/jpashop/domain/entity): Order, OrderItem 엔티티레벨 비즈니스 로직, 생성 로직, 조회 로직 추가
    ##Order, OrderItem 엔티티레벨 비즈니스 로직, 생성 로직, 조회 로직 추가
    
     - Order
    	 - 주문생성
    [생성 로직]
     	- 주문취소 [비즈니스 로직]
    		1. 주문의 상태를 취소 Set한다.
    		2. OrderItem 리스트를
    순회하며 모든 주문 상품을 취소한다.
    	 - 주문 가격 조회 [조회 로직]
    
     - OrderItem
    	 - 주문상품 생성
    [생성로직]
    	 - 주문상품 취소 [비즈니스 로직]
    		1. 주문상품의 Item의 재고를 주문상품의 수량만큼 되돌려 놓는다.
    (addStock)
    	 - 주문상품 가격 조회 [조회 로직]

--- src/main/java/jpabook/jpashop/domain/entity/Order.java
+++ src/main/java/jpabook/jpashop/domain/entity/Order.java
@@ -51,4 +51,46 @@ public class Order {
         delivery.setOrder(this);
     }
 
+
+    /// [생성 메서드]
+    // 현재 주문 시스템이 단순해서 orderItem 리스트를 그냥 넘기지만
+    // 실무에서는 DTO가 넘어오고 OrderItem을 직접 파싱해서 생성하는 등 많이 복잡해진다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+
+    }
+    /// [비즈니스 로직]
+    // 주문 취소
+    public void cancel(){
+        if(delivery.getDeliveryStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능 합니다.");
+        }
+        this.setOrderStatus(OrderStatus.CANCEL);
+        orderItems.forEach(
+                (item)->{
+                    item.cancel();
+                }
+        );
+    }
+
+    /// [조회 로직]
+    /**
+     * 전체 주문 가격 조회
+     * @return int
+     *  orderItems의 각 getOrderPrice를 sum한 결과
+     */
+    public int getTotalPrice(){
+        return orderItems.stream().mapToInt(OrderItem::getOrderPrice).sum();
+    }
+
 }
