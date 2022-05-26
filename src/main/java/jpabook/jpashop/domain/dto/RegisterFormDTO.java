d2b2d6d60c9b6637b29eb67605f19d051fa66b98
JunGeunHong1129 <gjhong1129@gmail.com>
2022-05-26 17:26:39 +0900

    feat(/src/main/java/jpabook/jpashop): MemberController, RegisterFormDTO 기능 추가
    ##MemberController, RegisterFormDTO 기능 추가
    
     - MemberController
    	 -
    createForm
    		맴버 생성 폼 Get 요청 처리 로직 추가
    	 - create
    		맴버 생성 Post 요청 처리 로직
    추가
     - RegisterFormDTO
    	맴버 생성 폼 전용 DTO,
    	맴버이름, 도시, 도로명, zipcode등 필드 추가

--- src/main/java/jpabook/jpashop/domain/dto/RegisterFormDTO.java
+++ src/main/java/jpabook/jpashop/domain/dto/RegisterFormDTO.java
@@ -1,4 +1,17 @@
 package jpabook.jpashop.domain.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
 public class RegisterFormDTO {
+
+    @NotEmpty
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
 }
--- src/main/java/jpabook/jpashop/web/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/web/controller/MemberController.java
@@ -1,12 +1,48 @@
 package jpabook.jpashop.web.controller;
 
 
+import jpabook.jpashop.domain.dto.RegisterFormDTO;
+import jpabook.jpashop.domain.entity.Address;
+import jpabook.jpashop.domain.entity.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm( Model model){
+
+        model.addAttribute("memberForm",new RegisterFormDTO());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Valid RegisterFormDTO form, BindingResult result){
+
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(new Address(form.getCity(),form.getStreet(),form.getZipcode()));
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 
 
