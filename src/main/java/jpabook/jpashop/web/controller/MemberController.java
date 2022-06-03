4fc7a2159f5eb76956ba20a44a26a275220a6974
hongjungeun <gjhong1129@gmail.com>
2022-06-04 08:26:40 +0900

    fix(/src/main/java/jpabook/jpashop/web/): MemberController, createMemberForm.html 데이터 바인딩 수정
    ##MemberController, createMemberForm.html 데이터 바인딩 수정
    
    	 -
    MemberController
    		 - create()
    			 데이터 모델 명이 일치 하지 않아 찾지 못하는 부분 수정
    		 -
    createForm()
    			 DTO모델명 변경에 따른 파생적 수정
    		 - memberList()
    			 멤버 리스트 조회 기능
    추가
    			 원래는 DTO를 보내야하며 페이징이 필수라는 점을 염두 할 것

--- src/main/java/jpabook/jpashop/web/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/web/controller/MemberController.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop.web.controller;
 
 
-import jpabook.jpashop.domain.dto.RegisterFormDTO;
+import jpabook.jpashop.domain.dto.MemberRegisterFormDTO;
 import jpabook.jpashop.domain.entity.Address;
 import jpabook.jpashop.domain.entity.Member;
 import jpabook.jpashop.service.MemberService;
@@ -10,9 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,29 +22,39 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /// 출력 또한 DTO를 뿌려야하는게 맞지만 단순한 예제라서 이렇게 진행되었다.
+    @GetMapping("/members")
+    public String memberList(Model model){
+        List<Member> members = memberService.findAllMember();
+        model.addAttribute("members",members);
+
+        return "members/memberList";
+    }
+
     @GetMapping(value = "/members/new")
     public String createForm( Model model){
 
-        model.addAttribute("memberForm",new RegisterFormDTO());
+        model.addAttribute("memberForm",new MemberRegisterFormDTO());
 
         return "members/createMemberForm";
     }
 
     @PostMapping(value = "/members/new")
-    public String create(@Valid RegisterFormDTO form, BindingResult result){
+    public String create(@Valid @ModelAttribute("memberForm") MemberRegisterFormDTO memberForm, BindingResult result){
 
         if(result.hasErrors()){
             return "members/createMemberForm";
         }
 
         Member member = new Member();
-        member.setName(form.getName());
-        member.setAddress(new Address(form.getCity(),form.getStreet(),form.getZipcode()));
+        member.setName(memberForm.getName());
+        member.setAddress(new Address(memberForm.getCity(),memberForm.getStreet(),memberForm.getZipcode()));
 
         memberService.join(member);
 
         return "redirect:/";
     }
+
 
 
 
